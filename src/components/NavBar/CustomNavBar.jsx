f3437f4a32376f36886e79a7f5526cf7857b5a23
Muhammad Suffian Apandi <mdsuffian83@hotmail.com>
2023-04-09 21:07:21 +0800

    test image

--- src/components/NavBar/CustomNavBar.jsx
+++ src/components/NavBar/CustomNavBar.jsx
@@ -35,7 +35,11 @@ export default function CustomNavBar({
       // bg={themeName}
       variant={'dark'}>
       <Navbar.Brand title="Better Life" className="d-block d-lg-none">
-        <img src="../../../images/UI/3.png" alt="BetterLife" className="bl-logo" />
+        <img
+          src="../../assets/images/UI/3.png"
+          alt="BetterLife"
+          className="bl-logo"
+        />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbarScroll" />
       {user && <Navbar.Brand>{welcomeMessage}</Navbar.Brand>}
